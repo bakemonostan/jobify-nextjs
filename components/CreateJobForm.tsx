0611a2b1ab16bdc939132468cd33b96fba981090
bakemonostan <ehizomokhomion@gmail.com>
2024-03-15 08:13:44 +0100

    add reusuable form component

--- components/CreateJobForm.tsx
+++ components/CreateJobForm.tsx
@@ -1,20 +1,16 @@
 "use client";
-
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-
-import { Button } from "@/components/ui/button";
-
+import { Form } from "@/components/ui/form";
+import { CustomFormField, CustomFormSelect } from "./FormComponent";
 import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "./ui/input";
+  JobStatus,
+  CreateAndEditJobType,
+  JobMode,
+  createAndEditJobSchema,
+} from "@/utils/types";
+import { Button } from "./ui/button";
 
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -23,31 +19,56 @@ const formSchema = z.object({
 });
 
 export default function CreateJobForm() {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<CreateAndEditJobType>({
+    resolver: zodResolver(createAndEditJobSchema),
     defaultValues: {
-      username: "",
+      position: "",
+      company: "",
+      location: "",
+      status: JobStatus.Pending,
+      mode: JobMode.FullTime,
     },
   });
-  function onSubmit(data: z.infer<typeof formSchema>) {
-    console.log(data);
+  function onSubmit(values: CreateAndEditJobType) {
+    console.log(values);
   }
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="Username" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="bg-muted p-8 rounded"
+      >
+        <h2 className="capitalize font-semibold text-4xl mb-6">add job</h2>
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 items-start">
+          {/* position */}
+          <CustomFormField name="position" control={form.control} />
+          {/* company */}
+          <CustomFormField name="company" control={form.control} />
+          {/* location */}
+          <CustomFormField name="location" control={form.control} />
+          {/* job status */}
+          <CustomFormSelect
+            name="status"
+            control={form.control}
+            labelText="job status"
+            items={Object.values(JobStatus)}
+          />
+          {/* job mode */}
+          <CustomFormSelect
+            name="mode"
+            control={form.control}
+            labelText="job mode"
+            items={Object.values(JobMode)}
+          />
+          <Button
+            type="submit"
+            className="self-end capitalize"
+            // disabled={isPending}
+          >
+            {/* {isPending ? 'loading' : 'create job'} */}
+            Submit
+          </Button>
+        </div>
       </form>
     </Form>
   );
